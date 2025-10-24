053a3c03a9472dda549918de446b56aafc74bc97
sh <shhang.2021@smu.edu.sg>
2025-10-24 09:14:47 +0800

    initial commit - remove storyboard

---
+++ ios-finWise/ios-finWise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-finWise
+//
+//  Created by Sing Hui Hang on 24/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
