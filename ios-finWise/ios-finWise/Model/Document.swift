9c53ef2fc0fd8804ab43376293af0589e3bf98d8
sh <shhang.2021@smu.edu.sg>
2025-10-24 10:30:07 +0800

    feat: added user and document models as structs (TODO)

---
+++ ios-finWise/ios-finWise/Model/Document.swift
@@ -0,0 +1,10 @@
+//
+//  Document.swift
+//  ios-finWise
+//
+//  Created by Sing Hui Hang on 24/10/25.
+//
+
+struct Document{
+    //TODO: Fill in document fields
+}
---
+++ ios-finWise/ios-finWise/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  ios-finWise
+//
+//  Created by Sing Hui Hang on 24/10/25.
+//
+
+struct User{
+    //Add more details later
+    let name: String
+    
+
+}
