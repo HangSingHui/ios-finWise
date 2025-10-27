4c6978dc5312875e18b35218ce06eaf5cdee882a
sh <shhang.2021@smu.edu.sg>
2025-10-27 18:13:44 +0800

    feat: added global app delegate variable for savedAnalysis, converted ProcessedDocument to Hashable so that it can be saved in savedAnalysis set, added loading view controller (yet to be coded)

---
+++ ios-finWise/ios-finWise/View/ResultsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoadingViewController.swift
+//  ios-finWise
+//
+//  Created by Sing Hui Hang on 27/10/25.
+//
+
+
+import UIKit
+
+class LoadingViewController: UIViewController{
+    //Add buttons
+    let homeButton = UIButton()
+    let saveButton = UIButton()
+    let shareButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureButtons()
+        
+    }
+    
+    private func configureButtons(){
+        
+    }
+}
