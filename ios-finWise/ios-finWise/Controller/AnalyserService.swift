dec26519d04a71278336f0c2f3786d6878a345f4
sh <shhang.2021@smu.edu.sg>
2025-10-27 10:54:21 +0800

    feat: added openai and lottie file packages

---
+++ ios-finWise/ios-finWise/Controller/AnalyserService.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyserService.swift
+//  ios-finWise
+//
+//  Created by Sing Hui Hang on 25/10/25.
+//
+
+class AnalyserService{
+    
+    // 1. Initialise OPENAI api
+    
+    //2. Set system prompt based on user configuration results - specify the structure to return so that it can be parsed properly
+    
+    //3. print response
+    
+    
+}
